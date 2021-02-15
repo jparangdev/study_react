70a9dc02721869cdb3c2aac6f1d148e028c99393
jparangdev <jparangdev@gmail.com>
2021-02-15 22:45:16 +0900

    환경변수 테스트

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
 
   return (
     <div className="App">
+      <div>
+          node_env => {process.env.NODE_ENV}<br />
+          data_api => {process.env.REACT_APP_DATA_API}<br />
+          login_api => %REACT_APP_LOGIN_API%
+      </div>
       <TodoList />
     </div>
   );
